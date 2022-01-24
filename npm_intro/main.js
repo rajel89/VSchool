fb90e11637cc8f3d376cbfb0315c9452d85a111d
Rajah Ello <rajah.ello@yahoo.com>
2022-01-23 20:01:41 -0600

    created the node calculator.

--- npm_intro/main.js
+++ npm_intro/main.js
@@ -1,6 +1,45 @@
-const readline = require("readline-sync");
-const num1 = parseInt(prompt('Enter the first number '));
-const num2 = parseInt(prompt('Enter the second number '));
+// var readlineSync = require('readline-sync')
 
-const sum = num1 + num2;
-console.log(num1 + num2);
+// // var name = readlineSync.question("what is your name? ")
+// // console.log("hellop " + name);
+
+
+
+var readlineSync = require('readline-sync');
+
+var Calculation = function(num1, num2, op) {
+  this.x = parseInt(num1);
+  this.y = parseInt(num2);
+  this.op = op;
+
+  this.result = function() {
+    var x = this.x;
+    var y = this.y;
+    var op = this.op;
+
+
+   
+    if (op === "A") {
+        var result = x + y;
+      } else if (op === "B") {
+        var result = x - y;
+      } else if (op === "C") {
+        var result = x * y;
+      } else if (op === "D") {
+        var result = x / y;
+      } else {
+        var result = "Error!"
+      }
+  
+      console.log("Result: " + result);
+    };
+  }
+  
+  var num1 = readlineSync.question("Please enter a number: ");
+  var num2 = readlineSync.question("Please enter another number: ");
+  var op = readlineSync.question("Please enter an operator: \n A to Add\n B to Sub\n C to Mul\n D to Div \n")
+
+  
+  var calc = new Calculation(num1, num2, op);
+  
+  calc.result();
