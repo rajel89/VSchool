4fdc238770c8bf15c895a008e88a5c2ee9f92c75
Rajah Ello <rajah.ello@yahoo.com>
2022-02-19 12:17:58 -0600

    dome-lymplics-start adding to the repo

--- moms-shopping-list-start/README.md
+++ moms-shopping-list-start/README.md
@@ -9,3 +9,11 @@ Look for a `.git`
 If you find it, do an `rm -rf .git`
 
 Use it as a springboard for your own Mom's shopping list app. The todos that are already in the HTML are there only to give an example of what the ones created in Part 1 should look like. Delete them from the HTML and make sure your code still works before moving on to Part 2.    
+
+
+References: 
+
+To Do List
+
+https://www.w3schools.com/howto/howto_js_todolist.asp?fbclid=IwAR18WTZlSfCw_W1h72FKsaDT3OJmNS6EixKDPLPDs5rRVSWQdBQggco0wUU
+
--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -2,14 +2,15 @@ const form = document.addItem;
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-
 });
 
-const itemInput = document.getElementById("title");
-const submit = document.getElementById("submit");
-const edit = document.getElementById("edit");
-const clear = document.getElementById("x");
+var itemInput = document.getElementById["add-todo"];
+let submit = document.getElementById("submit");
+let edit = document.getElementById("edit");
+let clear = document.getElementById("clear");
 
+
+//with this code, the submit button is being detected by the Even Listener//
 function addItem() {
   const currentItem = itemInput.value;
   const listItem = document.createElement("li");
@@ -17,12 +18,12 @@ function addItem() {
 
   list.appendChild(listItem);
 
-  itemInput.value = ""
+  itemInput.value = "";
 }
 
 submit.onClick = addItem;
 
-clear.onClick = function(){
-    list.innerHTML= "";
-    itemInput.value = "";
+clear.onClick = function () {
+  list.innerHTML = "";
+  itemInput.value = "";
 };
