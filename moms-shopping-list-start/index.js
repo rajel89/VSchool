b0bc1d4598f763b9b7a2648d6d7c3b2f92ea7ba9
Rajah Ello <rajah.ello@yahoo.com>
2022-03-02 00:46:32 -0600

    added event listener for submit button and arrow function to append to ul id=list

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -1,5 +1,27 @@
-window.addEventListener('load', () => {
-	const form = document.querySelector("#add-todo");
-	const input = document.querySelector("#title");
-	const itemList = document.querySelector("#list");
+window.addEventListener("load", () => {
+  const form = document.querySelector("#add-todo");
+  const input = document.querySelector("#title");
+  const itemList = document.querySelector("#list");
 
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const item = input.value;
+
+    const item_el = document.createElement("div");
+    item_el.classList.add("item");
+
+    const item_content_el = document.createElement("div");
+    item_content_el.classList.add("content");
+
+    item_el.appendChild(item_content_el);
+
+    const item_input_el = document.createElement("input");
+
+    item_content_el.appendChild(item_input_el);
+
+    itemList.appendChild(item_el);
+
+    input.value = "";
+  });
+});
