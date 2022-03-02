28ac0c211c169fcd8d51f0c295a3bf5e7f083a52
Rajah Ello <rajah.ello@yahoo.com>
2022-03-02 01:18:17 -0600

    formatted document for better layout and reading, no changes on the code.

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -16,62 +16,49 @@ window.addEventListener("load", () => {
 
     item_el.appendChild(item_content_el);
 
-    
-
     const item_input_el = document.createElement("input");
 
-    item_input_el.classList.add('text');
-		item_input_el.type = 'text';
-		item_input_el.value = item;
-		item_input_el.setAttribute('readonly', 'readonly');
-
-
-
+    item_input_el.classList.add("text");
+    item_input_el.type = "text";
+    item_input_el.value = item;
+    item_input_el.setAttribute("readonly", "readonly");
 
     item_content_el.appendChild(item_input_el);
 
+    const item_toAdd_el = document.createElement("div");
+    item_toAdd_el.classList.add("actions");
 
+    const item_edit_el = document.createElement("button");
+    item_edit_el.classList.add("edit");
+    item_edit_el.innerText = "Edit";
 
-
-    const item_toAdd_el = document.createElement('div');
-		item_toAdd_el.classList.add('actions');
-
-    const item_edit_el = document.createElement('button');
-		item_edit_el.classList.add('edit');
-		item_edit_el.innerText = 'Edit';
-
-		const item_delete_el = document.createElement('button');
-		item_delete_el.classList.add('x');
-		item_delete_el.innerText = 'x';
+    const item_delete_el = document.createElement("button");
+    item_delete_el.classList.add("x");
+    item_delete_el.innerText = "x";
 
     item_toAdd_el.appendChild(item_edit_el);
 
-    
+    item_toAdd_el.appendChild(item_delete_el);
 
-		item_toAdd_el.appendChild(item_delete_el);
-
-		item_el.appendChild(item_toAdd_el);
-
+    item_el.appendChild(item_toAdd_el);
 
     itemList.appendChild(item_el);
 
     input.value = "";
-    
-    item_edit_el.addEventListener('click', (e) => {
-			if (item_edit_el.innerText.toLowerCase() == "edit") {
-				item_edit_el.innerText = "Save";
-				item_input_el.removeAttribute("readonly");
-				item_input_el.focus();
-			} else {
-				item_edit_el.innerText = "Edit";
-				item_input_el.setAttribute("readonly", "readonly");
-			}
-		});
-    
-		item_delete_el.addEventListener('click', (e) => {
-			itemList.removeChild(item_el);
 
+    item_edit_el.addEventListener("click", (e) => {
+      if (item_edit_el.innerText.toLowerCase() == "edit") {
+        item_edit_el.innerText = "Save";
+        item_input_el.removeAttribute("readonly");
+        item_input_el.focus();
+      } else {
+        item_edit_el.innerText = "Edit";
+        item_input_el.setAttribute("readonly", "readonly");
+      }
+    });
+
+    item_delete_el.addEventListener("click", (e) => {
+      itemList.removeChild(item_el);
+    });
   });
-});
-
 });
