4def42be5649afdad2c74ef871529aa7e0fb339d
Rajah Ello <rajah.ello@yahoo.com>
2022-03-02 00:15:42 -0600

    added eventlistener and arrow function, query selector for the add-todo, title, and list

---
+++ moms-shopping-list-start/index.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+	const form = document.querySelector("#add-todo");
+	const input = document.querySelector("#title");
+	const itemList = document.querySelector("#list");
+
