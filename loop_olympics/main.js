1ccd7fd7a5289029892437a70347a65a3ef45616
Rajah Ello <rajah.ello@yahoo.com>
2022-03-20 00:24:08 -0500

    for loop to push every other fruit to an array.

--- loop_olympics/main.js
+++ loop_olympics/main.js
@@ -46,9 +46,9 @@ console.log("1. Write a for loop that will push the numbers 0 through 9 to an ar
 console.log("//2. Write a for loop that prints to the console only even numbers 0 through 100.")
 //2. Write a for loop that prints to the console only even numbers 0 through 100.
 
-// for (var i = 0; i <= 100; i++){
-//   console.log(i);
-// }
+for (var i = 0; i <= 100; i++){
+  console.log(i);
+}
 
 
 
