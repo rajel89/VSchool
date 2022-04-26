f72def9610d947c9fde561544ba41d5d7b526bfa
Rajah Ello <rajah.ello@yahoo.com>
2022-04-26 00:54:28 -0500

    delete game.js folder, added while function to player action.

--- colossal_rpg2/index.js
+++ colossal_rpg2/index.js
@@ -1,12 +1,23 @@
 const prompt = require('prompt-sync')();
 const readline = require('readline-sync');
-// const Game = require('./game')
+
 
 // //Console must greet player with a fun message
 // //Console must ask for the player's name and store it
 
 const playerName = readline.question("Hello, what is your name? \n");
 console.log("\nHello " + playerName + ", you are about to play the 'Colossal Adventure RPG'\n");
+
+
+const player = {
+  name: (''),
+  hp: 100,
+  inventory: []
+}
+
+const enemy = {
+  hp: 100
+}
 
 
 const gameModeOptions = {
@@ -24,7 +35,12 @@ while (1) {
           console.log('------------ Game Terminated ------------\n')
           break;
       } else {
-          const newGame = new Game(gameModeOptions[selectedGameMode]);
+          while(player.hp > 0 ){
+            let action = readline.keyIn('\nPress W to walk or P to check Health and Inventory. ').toLowerCase();
+            if(action === 'p'){
+              console.log(playerName)
+            }
+          }
           break;
       }
       
