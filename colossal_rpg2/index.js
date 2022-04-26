3856ce8b466fea5ad5d161f9b18cb1fe2799e28e
Rajah Ello <rajah.ello@yahoo.com>
2022-04-26 00:23:02 -0500

    created game.js folder

--- colossal_rpg2/index.js
+++ colossal_rpg2/index.js
@@ -1,16 +1,33 @@
 const prompt = require('prompt-sync')();
-const readlineSync = require('readline-sync');
+const readline = require('readline-sync');
+// const Game = require('./game')
+
+// //Console must greet player with a fun message
+// //Console must ask for the player's name and store it
+
+const playerName = readline.question("Hello, what is your name? \n");
+console.log("\nHello " + playerName + ", you are about to play the 'Colossal Adventure RPG'\n");
 
 
-//Console must greet player with a fun message
-//Console must ask for the player's name and store it
-const userName = readlineSync.question("Hello, what is your name? \n");
-console.log("\nHello " + userName + ", you are about to play the colosal RPG, please select from the following. \n");
+const gameModeOptions = {
+  A: "Play",
+  B: "Exit"
+}
 
+while (1) {
+  console.log('------------ Select Actions ------------\n')
+  console.log("A: Play\nB: Exit\n");
 
-
-
-
-
-
+  let selectedGameMode = prompt("");
+  if (selectedGameMode === 'A' || selectedGameMode === 'B') {
+      if (selectedGameMode === 'B') {
+          console.log('------------ Game Terminated ------------\n')
+          break;
+      } else {
+          const newGame = new Game(gameModeOptions[selectedGameMode]);
+          break;
+      }
+      
+  }
+}
 
