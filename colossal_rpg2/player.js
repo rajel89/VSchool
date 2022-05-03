10a1bd2ee83604f4df5fea2dcb26c65f7bec9ae2
Rajah Ello <rajah.ello@yahoo.com>
2022-05-02 23:38:04 -0500

    added constructor function in player.js

--- colossal_rpg2/player.js
+++ colossal_rpg2/player.js
@@ -1,4 +1,25 @@
 class Player{
+    constructor()
+    {
+        this.name = "";
+        this.hp = 100;
+        this.prizeList = [];
+    }
+
+    setName (name){
+        this.name = name;
+    }
+
+    getName(){
+        return this.name
+    }
+    
+    getHP(){
+        return this.hp;
+
+    }
+    
+
 
 }
 
