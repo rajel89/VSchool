4a78b519f96ba4a574b26a7d5dc0912b7220fc47
Rajah Ello <rajah.ello@yahoo.com>
2022-05-10 00:37:48 -0500

    added plase holders for multiply, and substract operators in index.html file.

---
+++ simple_html_calculator/index.js
@@ -0,0 +1,11 @@
+// const form = document.calculator
+
+// form.addEventListener("add", function (e){
+//     e.preventDefault()
+// })
+
+
+
+document.addEventListener.addEventListener("Add", function(e){
+    e.preventDefault()
+})
