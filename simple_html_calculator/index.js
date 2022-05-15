9c1c6c9dee7185a4cddc6b8176f008a6bdedc191
Rajah Ello <rajah.ello@yahoo.com>
2022-05-14 23:51:23 -0500

    added js operations for addition and multiplication using getElementById.

--- simple_html_calculator/index.js
+++ simple_html_calculator/index.js
@@ -1,8 +1,16 @@
+
 function add()
 {
     var numOne, numTwo, sum;
     numOne = parseInt(document.getElementById("num1").value);
     numTwo = parseInt(document.getElementById("num2").value);
     sum = numOne + numTwo;
-    document.getElementById("result").value = sum;
+    document.getElementById("addResult").value = sum;
+}
+
+function multiplyBy()
+{
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("multResult").value = num1 * num2;
 }
