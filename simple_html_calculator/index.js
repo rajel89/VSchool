013da6ae16a8d3c072e6f7d067fd77bf92a1f11d
Rajah Ello <rajah.ello@yahoo.com>
2022-05-15 00:10:37 -0500

    added function to substract.

--- simple_html_calculator/index.js
+++ simple_html_calculator/index.js
@@ -1,16 +1,21 @@
-
-function add()
-{
-    var numOne, numTwo, sum;
-    numOne = parseInt(document.getElementById("num1").value);
-    numTwo = parseInt(document.getElementById("num2").value);
-    sum = numOne + numTwo;
-    document.getElementById("addResult").value = sum;
+function add() {
+  var numOne, numTwo, sum;
+  numOne = parseInt(document.getElementById("addNum1").value);
+  numTwo = parseInt(document.getElementById("addNum2").value);
+  sum = numOne + numTwo;
+  document.getElementById("addResult").value = sum;
 }
 
-function multiplyBy()
-{
-    num1 = document.getElementById("firstNumber").value;
-    num2 = document.getElementById("secondNumber").value;
-    document.getElementById("multResult").value = num1 * num2;
+function multiplyBy() {
+  num1 = document.getElementById("multNum1").value;
+  num2 = document.getElementById("multNum2").value;
+  document.getElementById("multResult").value = num1 * num2;
+}
+
+function subBy() {
+  var numOne, numTwo, sum;
+  numOne = parseInt(document.getElementById("subNum1").value);
+  numTwo = parseInt(document.getElementById("subNum2").value);
+  sum = numOne - numTwo;
+  document.getElementById("subResult").value = sum;
 }
