f3f6324d7f7de9b755fdd4a650ee97810f4c26b6
Rajah Ello <rajah.ello@yahoo.com>
2022-06-05 15:13:57 -0500

    made array of number into string of numbers

--- array_map_exercises/index.js
+++ array_map_exercises/index.js
@@ -9,3 +9,13 @@ function doubleNumbers(arr){
   
   console.log(doubleNumbers([2, 5, 100])); // [4, 10, 200]
 
+//2) Take an array of numbers and make them strings
+
+function stringItUp(arr){
+    //your code here
+    let strArr = arr.map(String);
+    return strArr;
+
+}
+console.log(stringItUp([2, 5, 100]));//["2", "5", "100"]
+
