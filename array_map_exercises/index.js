d14b28bfc64b9cb158ad4d1aaac0812db306d421
Rajah Ello <rajah.ello@yahoo.com>
2022-06-05 15:48:18 -0500

    used IfElse condition to filter name

--- array_map_exercises/index.js
+++ array_map_exercises/index.js
@@ -1,37 +1,30 @@
 //1) Make an array of numbers that are doubles of the first array
+function doubleNumbers(arr) {
+  // your code here
 
-function doubleNumbers(arr){
-    // your code here
+  let numbMultiplier = arr.map((num) => num * 2);
+  return numbMultiplier;
+}
 
-    let numbMultiplier = arr.map(num => num * 2);
-    return numbMultiplier;
-  }
-  
-  console.log(doubleNumbers([2, 5, 100])); // [4, 10, 200]
+console.log(doubleNumbers([2, 5, 100])); // [4, 10, 200]
 
 //2) Take an array of numbers and make them strings
-
-function stringItUp(arr){
-    //your code here
-    let strArr = arr.map(String);
-    return strArr;
-
+function stringItUp(arr) {
+  //your code here
+  let strArr = arr.map(String);
+  return strArr;
 }
-console.log(stringItUp([2, 5, 100]));//["2", "5", "100"]
-
+console.log(stringItUp([2, 5, 100])); //["2", "5", "100"]
 
 //3) Capitalize the first letter of each name and make the rest of the characters lowercase
+function capitalizeNames(arr) {
+  //your code here
 
-function capitalizeNames(arr){
-    //your code here
+  let firstLetterCap = arr.map(function (string) {
+    return string.substr(0, 1).toUpperCase() + string.substr(1).toLowerCase();
+  });
 
-    let firstLetterCap = arr.map(function (string){
-        return string.substr(0,1).toUpperCase() + string.substr(1).toLowerCase();
-    })
-
-    return firstLetterCap
-    
-
+  return firstLetterCap;
 }
 
 console.log(capitalizeNames(["john", "Jacob", "jinGleHeimer", "schmidt"]));
@@ -39,28 +32,64 @@ console.log(capitalizeNames(["john", "Jacob", "jinGleHeimer", "schmidt"]));
 //["John", "Jacob", "Jingleheimer", "Schmidt"]
 
 
-//4) Make an array of strings of the names
 
-function namesOnly(arr){
+//4) Make an array of strings of the names
+function namesOnly(arr) {
+  //your code here
+
+  let onlyNameArr = arr.map((n) => n.name);
+  return onlyNameArr;
+}
+console.log(
+  namesOnly([
+    {
+      name: "Angelina Jolie",
+      age: 80,
+    },
+    {
+      name: "Eric Jones",
+      age: 2,
+    },
+    {
+      name: "Paris Hilton",
+      age: 5,
+    },
+    {
+      name: "Kayne West",
+      age: 16,
+    },
+    {
+      name: "Bob Ziroll",
+      age: 100,
+    },
+  ])
+);
+
+//["Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]
+
+
+//5) Make an array of strings of the names saying whether or not they can go to The Matrix
+function makeString(arr){
     //your code here
 
-    let onlyNameArr = arr.map(n => n.name)
-    return onlyNameArr
-
+    let reFormArr = arr.map(arr =>{
+        if(arr.age>=18){
+            console.log(arr.name + " can go to The Matrix")
+        }
+        else{
+            console.log(arr.name + " is under age!!");
+        }
+    })
 
 }
 
-console.log(namesOnly([
+console.log(makeString([
     {
         name: "Angelina Jolie",
         age: 80
     },
     {
         name: "Eric Jones",
-        age: 2   
-    },
-    {
-        name: "Paris Hilton",
         age: 5
     },
     {
@@ -72,6 +101,10 @@ console.log(namesOnly([
         age: 100
     }
 ]));
+// ["Angelina Jolie can go to The Matrix",
+// "Eric Jones is under age!!",
+// "Paris Hilton is under age!!",
+// "Kayne West is under age!!",
+// "Bob Ziroll can go to The Matrix"]
 
-//["Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]
 
