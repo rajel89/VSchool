0756f140ea788123d7edfaecf55a2f6426d961bc
Rajah Ello <rajah.ello@yahoo.com>
2022-06-05 15:39:58 -0500

    extracted name form the array

--- array_map_exercises/index.js
+++ array_map_exercises/index.js
@@ -37,3 +37,41 @@ function capitalizeNames(arr){
 console.log(capitalizeNames(["john", "Jacob", "jinGleHeimer", "schmidt"]));
 //output;
 //["John", "Jacob", "Jingleheimer", "Schmidt"]
+
+
+//4) Make an array of strings of the names
+
+function namesOnly(arr){
+    //your code here
+
+    let onlyNameArr = arr.map(n => n.name)
+    return onlyNameArr
+
+
+}
+
+console.log(namesOnly([
+    {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2   
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 16
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+]));
+
+//["Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]
+
