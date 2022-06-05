2c4b4c502c1a7ec58235d50fe34794a9eb896ea5
Rajah Ello <rajah.ello@yahoo.com>
2022-06-05 15:08:44 -0500

    multiplied array of numbers using the .map() function.

--- array_map_exercises/index.js
+++ array_map_exercises/index.js
@@ -1,2 +1,11 @@
-console.log("Hello World!")
+//1) Make an array of numbers that are doubles of the first array
+
+function doubleNumbers(arr){
+    // your code here
+
+    let numbMultiplier = arr.map(num => num * 2);
+    return numbMultiplier;
+  }
+  
+  console.log(doubleNumbers([2, 5, 100])); // [4, 10, 200]
 
