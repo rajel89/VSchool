9f93bdfc5f16c2d565fcf281c94d277e381055e7
Rajah Ello <rajah.ello@yahoo.com>
2022-06-05 15:25:44 -0500

    set name to show first letter in uppercase.

--- array_map_exercises/index.js
+++ array_map_exercises/index.js
@@ -19,3 +19,21 @@ function stringItUp(arr){
 }
 console.log(stringItUp([2, 5, 100]));//["2", "5", "100"]
 
+
+//3) Capitalize the first letter of each name and make the rest of the characters lowercase
+
+function capitalizeNames(arr){
+    //your code here
+
+    let firstLetterCap = arr.map(function (string){
+        return string.substr(0,1).toUpperCase() + string.substr(1).toLowerCase();
+    })
+
+    return firstLetterCap
+    
+
+}
+
+console.log(capitalizeNames(["john", "Jacob", "jinGleHeimer", "schmidt"]));
+//output;
+//["John", "Jacob", "Jingleheimer", "Schmidt"]
