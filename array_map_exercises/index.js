2009782b4cc15b431e7510d787c3fea2d3fc6375
Rajah Ello <rajah.ello@yahoo.com>
2022-06-05 23:20:10 -0500

    mane array of names with h tages.

--- array_map_exercises/index.js
+++ array_map_exercises/index.js
@@ -108,3 +108,42 @@ console.log(makeString([
 // "Bob Ziroll can go to The Matrix"]
 
 
+//6) Make an array of the names in h1s, and the ages in h2s
+
+
+function readyToputIntheDOM(arr){
+    //your code here
+    let hTageArr = arr.map(arr => {
+        console.log("<h1>"+arr.name+"</h1>"+"<h2>"+arr.age+"</h2>")
+    })
+
+}
+console.log(readyToputIntheDOM([
+    {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 5
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+
+]));
+
+// ["<h1>Angelina Jolie</h1><h2>80</h2>",
+// "<h1>Eric Jones</h1><h2>2</h2>",
+// "<h1>Paris Hilton</h1><h2>5</h2>",
+// "<h1>Kayne West</h1><h2>16</h2>",
+// "<h1>Bob Ziroll</h1><h2>100</h2>"]
