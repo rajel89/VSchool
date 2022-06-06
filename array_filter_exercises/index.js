216b2a8e8a41413f31925df6adb27831bfc0a7bf
Rajah Ello <rajah.ello@yahoo.com>
2022-06-06 00:41:06 -0500

    only return even number from array of numbers.

--- array_filter_exercises/index.js
+++ array_filter_exercises/index.js
@@ -1,17 +1,24 @@
 //1) Given an array of numbers, return a new array that has only the 
 //numbers that are 5 or greater.
 
-
 function fiveAndGreaterOnly(arr){
     //your code here
 
     let newNumber = arr.filter (fiveAndGreaterOnly => fiveAndGreaterOnly >= 5)
     return newNumber
-
 }
-
-
-
 //test
-
 console.log(fiveAndGreaterOnly([3,6,8,2])); ///[6,8]
+
+//2) Given an array of numbers, return a new array that only 
+//includes the even numbers.
+
+function evensOnly(arr){
+    //your code here
+    let evenNumber = arr.filter(evensOnly => evensOnly % 2 === 0)
+    return evenNumber
+}
+//test
+console.log(evensOnly([3,6,8,2])); ///[6,8,2]
+
+
