0338734651dc05daba09d09c38c61939e977272c
Rajah Ello <rajah.ello@yahoo.com>
2022-06-14 01:11:35 -0500

    .find() syntax.

--- foreach_find_findindex/index.js
+++ foreach_find_findindex/index.js
@@ -32,7 +32,31 @@ console.log(newArr)
 
 
 
-// .find()
+// .find() 
+
+
+const result1 = users.find(function (user){
+    if(user.name[0] === "j"){ // <--- user.name[0] means to look for the name with the first string "j", in this case it will return "joe" and will look no further."
+    return user
+}
+})
+
+console.log(result1)
+
+
+const result2 = users.find(function(user){
+    if(user.name === "julie"){ // <--- user.name ===, instructs the syntax to look for a specific name in the array of users."
+        return user
+    }
+})
+
+console.log(result2)
+
+
+
+// .findIndex()
+
+
 
 
 
