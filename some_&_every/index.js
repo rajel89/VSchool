028b47adefe3e3b83740efb674af8a833d7f2c15
Rajah Ello <rajah.ello@yahoo.com>
2022-06-14 01:34:48 -0500

    .every()

--- some_&_every/index.js
+++ some_&_every/index.js
@@ -14,3 +14,11 @@ console.log(result)
 
 // .every()
 
+
+const result1 = names.every(function(name){
+    if(name[0] === "J"){ // <--- will only return true if every object in the array starts with the letter "J". In this case, return is false because "Steve" starts with "S".
+        return true
+    }
+})
+
+console.log(result1)
