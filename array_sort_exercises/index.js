ed0c39dabf7b12fc32baca8e7ae08cbbf5c423d7
Rajah Ello <rajah.ello@yahoo.com>
2022-07-02 15:32:08 -0500

    Array sort exercises

---
+++ array_sort_exercises/index.js
@@ -0,0 +1,11 @@
+//1) Sort an array from smallest number to largest
+
+function leastToGreatest(arr) {
+
+    // your code here
+
+    
+  }
+  
+  console.log(leastToGreatest([1, 3, 5, 2, 90, 20])); // [1, 2, 3, 5, 20, 90]
+  
