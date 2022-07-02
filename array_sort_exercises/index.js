1609beca428319fd4ccf93f4a69974f8376a8dc2
Rajah Ello <rajah.ello@yahoo.com>
2022-07-02 16:03:55 -0500

    greatest to least

--- array_sort_exercises/index.js
+++ array_sort_exercises/index.js
@@ -7,3 +7,15 @@ function leastToGreatest(arr1) {
 }
 
 console.log(leastToGreatest([1, 3, 5, 2, 90, 20])); // [1, 2, 3, 5, 20, 90]
+
+
+//2) Sort an array from largest number to smallest
+function greatestToLeast(arr2) {
+    // your code here
+    return arr2.sort(function(a,b){
+        return b -a
+    })
+
+  }
+  
+  console.log(greatestToLeast([1, 3, 5, 2, 90, 20])); // [90, 20, 5, 3, 2, 1]
