d61d24a28f865e23ecc8ded6d52d093d7bb93499
Rajah Ello <rajah.ello@yahoo.com>
2022-07-02 16:24:41 -0500

    sort alphabetical order

--- array_sort_exercises/index.js
+++ array_sort_exercises/index.js
@@ -1,33 +1,49 @@
 //1) Sort an array from smallest number to largest
-
+console.log("1) Sort an array from smallest number to largest")
 function leastToGreatest(arr1) {
-  return arr1.sort(function(a,b){
-    return a - b
-  })
+  return arr1.sort(function (a, b) {
+    return a - b;
+  });
 }
-
 console.log(leastToGreatest([1, 3, 5, 2, 90, 20])); // [1, 2, 3, 5, 20, 90]
 
 
+
+
 //2) Sort an array from largest number to smallest
+console.log("2) Sort an array from largest number to smallest")
 function greatestToLeast(arr2) {
-    // your code here
-    return arr2.sort(function(a,b){
-        return b -a
+  // your code here
+  return arr2.sort(function (a, b) {
+    return b - a;
+  });
+}
+console.log(greatestToLeast([1, 3, 5, 2, 90, 20])); // [90, 20, 5, 3, 2, 1]
+
+
+
+
+//3) Sort an array from shortest string to longest
+console.log("3) Sort an array from shortest string to longest")
+function lengthSort(arr3) {
+  // your code here
+  return arr3.sort((a, b) => a.length - b.length);
+}
+console.log(lengthSort(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "wolf", "eaten", "family"]
+
+
+
+
+//4) Sort an array alphabetically
+console.log("4) Sort an array alphabetically")
+function alphabetical(arr4) {
+    // return arr4.sort((a,b)=> a - b)
+    return arr4.sort(function(a,b){
+        return a === b ? 0 : a < b ? -1 : 1;
     })
-
-  }
-  
-  console.log(greatestToLeast([1, 3, 5, 2, 90, 20])); // [90, 20, 5, 3, 2, 1]
+}
+console.log(alphabetical(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "eaten", "family", "wolf"]
 
 
-  //3) Sort an array from shortest string to longest
-
-  function lengthSort(arr3) {
-    // your code here
-    return arr3.sort((a,b)=> a.length - b.length);
 
 
-  }
-  
-  console.log(lengthSort(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "wolf", "eaten", "family"]
