26f2def38b738b996293ba809595bcedaa338449
Rajah Ello <rajah.ello@yahoo.com>
2022-07-02 20:46:00 -0500

    Given an array of all your wishlist items, figure out how much it would cost to just buy everything at once

--- array_reduce_exercises/index.js
+++ array_reduce_exercises/index.js
@@ -60,3 +60,24 @@ var voters = [
   { name: "Zack", age: 19, voted: false },
 ];
 console.log(totalVotes(voters)); // 7
+
+//4) Given an array of all your wishlist items, figure out how much it would cost to just buy everything at once
+console.log("4) Given an array of all your wishlist items, figure out how much it would cost to just buy everything at once")
+
+function shoppingSpree(arr4) {
+  // your code here
+    const totalPrice = wishlist.map(prices => Number(prices.price))
+    .reduce((sumOfPrices, pricing) => sumOfPrices + pricing)
+    return totalPrice
+
+}
+
+var wishlist = [
+  { title: "Tesla Model S", price: 90000 },
+  { title: "4 carat diamond ring", price: 45000 },
+  { title: "Fancy hacky Sack", price: 5 },
+  { title: "Gold fidgit spinner", price: 2000 },
+  { title: "A second Tesla Model S", price: 90000 },
+];
+
+console.log(shoppingSpree(wishlist)); // 227005
