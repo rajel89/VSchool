41a6b077b7bc3db51dcdafad119fe9f1a3e3b8b1
Rajah Ello <rajah.ello@yahoo.com>
2022-07-02 19:05:54 -0500

    no change just formated

--- reduce/index.js
+++ reduce/index.js
@@ -1,84 +1,75 @@
-
 // 1 - reduce the array of numbers into a sum of all the numbers
-console.log("1 - reduce the array of numbers into a sum of all the numbers")
-const nums = [1,2,3,4,5,6,7,8,9,10]
+console.log("1 - reduce the array of numbers into a sum of all the numbers");
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const result1 = nums.reduce(function(final,num){
-    final += num
-    return final
-})
+const result1 = nums.reduce(function (final, num) {
+  final += num;
+  return final;
+});
 
-console.log(result1)
-
-
+console.log(result1);
 
 // 2 - reduce the users to an array of strings of the user's first and last names
-console.log("2 - reduce the users to an array of strings of the user's first and last names")
+console.log(
+  "2 - reduce the users to an array of strings of the user's first and last names"
+);
 const users = [
-    { fName: "joe", lName: "smithy" },
-    { fName: "tina", lName: "johnson" },
-    { fName: "rick", lName: "sanchez" }
-]
+  { fName: "joe", lName: "smithy" },
+  { fName: "tina", lName: "johnson" },
+  { fName: "rick", lName: "sanchez" },
+];
 
-const result = users.reduce(function(final,user){
-    final.push(user.fName + "" + user.lName)
-    return final
-}, [])
+const result = users.reduce(function (final, user) {
+  final.push(user.fName + "" + user.lName);
+  return final;
+}, []);
 
-console.log(result)
-
+console.log(result);
 
 // 3 - reduce the array into a count of how many people voted
-console.log("3 - reduce the array into a count of how many people voted")
+console.log("3 - reduce the array into a count of how many people voted");
 
 const voters = [
-    { name: "steve", voted: true },
-    { name: "janet", voted: true },
-    { name: "rebecca", voted: false },
-    { name: "harvey", voted: true },
-]
+  { name: "steve", voted: true },
+  { name: "janet", voted: true },
+  { name: "rebecca", voted: false },
+  { name: "harvey", voted: true },
+];
 
-const voteCount = voters.reduce(function(final,voters){
-    if(voters.voted){
-        final++
-    }
-    return final
-},0)
+const voteCount = voters.reduce(function (final, voters) {
+  if (voters.voted) {
+    final++;
+  }
+  return final;
+}, 0);
 
-console.log(voteCount)
+console.log(voteCount);
 
+const didntVoteCount = voters.reduce(function (final, voters) {
+  if (!voters.voted) {
+    // <--- the "!"  means the take the vote = false or to return the number of people who did not vote.
+    final++;
+  }
+  return final;
+}, 0);
 
-const didntVoteCount = voters.reduce(function(final, voters){
-    if(!voters.voted){ // <--- the "!"  means the take the vote = false or to return the number of people who did not vote.
-        final++
-    }
-    return final
-},0)
-
-console.log(didntVoteCount)
-
-
-
+console.log(didntVoteCount);
 
 // 4 - return an object that has a count of both who voted and who didnt
+console.log(
+  "4 - return an object that has a count of both who voted and who didnt"
+);
 
-
-const voterObj = voters.reduce(function(final, voter){
-    if(voter.voted){
-        final.didVote++
-    }else{
-        final.didntVote++
+const voterObj = voters.reduce(
+  function (final, voter) {
+    if (voter.voted) {
+      final.didVote++;
+    } else {
+      final.didntVote++;
     }
-    return final
+    return final;
+  },
+  { didVote: 0, didntVote: 0 }
+);
 
-},{didVote: 0, didntVote: 0})
-
-console.log(voterObj)
-
-
-
-
-
-
-
-
+console.log(voterObj);
