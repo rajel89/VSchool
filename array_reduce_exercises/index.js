01c92064ae00e88ec005c46e1123690255e5c719
Rajah Ello <rajah.ello@yahoo.com>
2022-07-02 19:36:27 -0500

    Turn an array of numbers into a total of all the numbers

--- array_reduce_exercises/index.js
+++ array_reduce_exercises/index.js
@@ -1,1 +1,14 @@
-console.log("Array Reduce Exercises")
+console.log("Array Reduce Exercises");
+//1) Turn an array of numbers into a total of all the numbers
+
+console.log("1) Turn an array of numbers into a total of all the numbers");
+
+function total(arr) {
+  // your code here
+  return arr.reduce(function (final, num) {
+    final += num;
+    return final;
+  });
+}
+
+console.log(total([1, 2, 3])); // 6
