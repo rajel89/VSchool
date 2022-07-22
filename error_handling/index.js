bc9aec686dfb91721f36fa8e514194aa303544e3
Rajah Ello <rajah.ello@yahoo.com>
2022-07-22 00:29:12 -0500

    error handling try block

--- error_handling/index.js
+++ error_handling/index.js
@@ -1,1 +1,22 @@
-console.log ("Hello World")
+try {
+    // try out some code
+    // if an error occurs (if an erro is thrown)
+
+    if(2 === 2){
+        // throw "Hey I am an error!"
+
+        throw new Error ("Hey I am an error")
+    }
+
+}
+
+catch(err){
+    //hand the error
+
+    console.log(err)
+}
+
+finally{
+    console.log("I am running no matter what!")
+}
+
