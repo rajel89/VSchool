c458e70a4c7da67572b4344e69884be82a1a55d5
Rajah Ello <rajah.ello@yahoo.com>
2022-07-22 23:10:03 -0500

    error handling

--- error_handling/index.js
+++ error_handling/index.js
@@ -20,3 +20,24 @@ finally{
     console.log("I am running no matter what!")
 }
 
+
+
+
+var input = "";
+
+try{
+    if(input == ""){
+        throw "Input can not be empty";
+
+    }else if(input.length > 5){
+        throw "input is to big";
+    }else{
+        console.log("Your code runs fine!");
+    }
+
+}catch(err){
+    console.log(err);
+}finally{
+    console.log("Run no matter what");
+    console.log("and the input was " + input);
+}
