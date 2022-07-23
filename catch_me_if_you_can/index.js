f95a887c78022faf760c6de41c73d258330e8f77
Rajah Ello <rajah.ello@yahoo.com>
2022-07-23 00:56:45 -0500

    let total = sum (4, 4)

--- catch_me_if_you_can/index.js
+++ catch_me_if_you_can/index.js
@@ -7,11 +7,9 @@
 let total = sum(4, 4);
 
 function sum (x ,y){
-    //check data type frist and throw error
+    //check data type first and throw error
+   
     return x + y;
-
-
-
 
 }
 
@@ -27,13 +25,14 @@ console.log(total)
 
 
 
+
 //2a) Given a user object, write a function called login that takes a username and password as parameters. 
 //Throw an error if either of them don't match. Otherwise, log to the console a message saying "login successful!"
 
 
 
-// var user = {username: "sam", password: "123abc"};
-// function login(username, password){
+var user = {username: "sam", password: "123abc"};
+function login(username, password){
     
 
 // }
