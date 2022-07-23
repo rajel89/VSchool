1e87cf069b48d21ea1dd4c3854fe0ea8fe8e451a
Rajah Ello <rajah.ello@yahoo.com>
2022-07-23 00:32:18 -0500

    sum function

--- catch_me_if_you_can/index.js
+++ catch_me_if_you_can/index.js
@@ -1,11 +1,21 @@
-//1a) Write a function that returns the sum of two numbers. Throw an error if either argument is not of the data type number.
 
+
+
+
+//1a) Write a function that returns the sum of two numbers. Throw an error if either argument is not of the data type "number".
+
+let total = sum(4, 4);
 
 function sum (x ,y){
     //check data type frist and throw error
-
     return x + y;
+
+
+
+
 }
+
+console.log(total)
 
 
 
@@ -20,12 +30,14 @@ function sum (x ,y){
 //2a) Given a user object, write a function called login that takes a username and password as parameters. 
 //Throw an error if either of them don't match. Otherwise, log to the console a message saying "login successful!"
 
-var user = {username: "sam", password: "123abc"};
-function login(username, password){
+
+
+// var user = {username: "sam", password: "123abc"};
+// function login(username, password){
     
 
-}
-console.log(login)
+// }
+// console.log(login)
 
 
 
--- error_handling/index.js
+++ error_handling/index.js
@@ -1,43 +1,33 @@
 try {
-    // try out some code
-    // if an error occurs (if an erro is thrown)
+  // try out some code
+  // if an error occurs (if an erro is thrown)
 
-    if(2 === 2){
-        // throw "Hey I am an error!"
+  if (2 === 2) {
+    // throw "Hey I am an error!"
 
-        throw new Error ("Hey I am an error")
-    }
+    throw new Error("Hey I am an error");
+  }
+} catch (err) {
+  //hand the error
 
+  console.log(err);
+} finally {
+  console.log("I am running no matter what!");
 }
-
-catch(err){
-    //hand the error
-
-    console.log(err)
-}
-
-finally{
-    console.log("I am running no matter what!")
-}
-
-
-
 
 var input = "";
 
-try{
-    if(input == ""){
-        throw "Input can not be empty";
-
-    }else if(input.length > 5){
-        throw "input is to big";
-    }else{
-        console.log("Your code runs fine!");
-    }
-
-}catch(err){
-    console.log(err);
-}finally{
-    console.log("Run no matter what");
-    console.log("and the input was " + input);
+try {
+  if (input == "") {
+    throw "Input can not be empty";
+  } else if (input.length > 5) {
+    throw "input is to big";
+  } else {
+    console.log("Your code runs fine!");
+  }
+} catch (err) {
+  console.log(err);
+} finally {
+  console.log("Run no matter what");
+  console.log("and the input was " + input);
 }
