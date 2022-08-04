6ca04768f689ba19b774a7cdce5e724df42bcec6
rajel89 <rajah.ello@yahoo.com>
2022-08-04 00:32:38 -0500

    unit testing

---
+++ Level 2/unit_testing/index.js
@@ -0,0 +1,15 @@
+function sum(num1, num2){   
+    return num1 + num2
+
+}
+
+// will have to values called ""expected", "actual"
+const actual = sum(10,10)
+const expected = 20
+
+if (actual !== expected){
+    throw new Error (`TEST FAILED: Expected ${expected}, but received ${actual}.`)
+}else{
+    console.log(`TEST PASSED: ${expected} === ${actual}`)
+}
+
