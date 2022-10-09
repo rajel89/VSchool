63810a73bbc0a00c532d5717a98bc6f33722b52c
Rajah Ello <rajah.ello@yahoo.com>
2022-10-08 23:05:01 -0500

    introduction to axios

--- Level 2/introduction_to_axios/index.js
+++ Level 2/introduction_to_axios/index.js
@@ -1,10 +1,29 @@
 // Get request with axios
 
-//url: https://api.vschool.io/<yourname>/todo
+//url: https://api.vschool.io/rajel/todo
 
+// Get All - is to pull all data from the end point "/todo"
+// axios is promise base. Once axios GET the data, what do we want to do with it?
 
-// Get All
+axios.get("https://api.vschool.io/rajel/todo")
+    .then(response => console.log(response.data))
+    .catch(error => console.log(error))
 
+// Get One - is to pull one data from the list of todo's by using the object id "631ecabf18d1a05bceae7455"
 
+axios.get("https://api.vschool.io/rajel/todo/631ecabf18d1a05bceae7455")
+.then(response => console.log(response.data))
+.catch(error => console.log(error))
 
-// Get One
+// Posting result into the web page
+
+axios.get("https://api.vschool.io/rajel/todo")
+  .then((response) => {
+    for (let i = 0; i < response.data.length; i++) {
+      const h1 = document.createElement('h1')
+      h1.textContent = response.data[i].title
+      document.body.appendChild(h1);
+    }
+  })
+  .catch(error => console.log(error))
+
