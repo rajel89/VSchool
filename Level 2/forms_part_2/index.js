8f6e04e2e26161ab0a41c52df81941b8d83163b8
Rajah Ello <rajah.ello@yahoo.com>
2022-10-23 01:50:11 -0500

    console.log input values into terminal

--- Level 2/forms_part_2/index.js
+++ Level 2/forms_part_2/index.js
@@ -1,19 +1,21 @@
-const form = document.myForm
+const form = document.myForm;
 
-form.addEventListener("submit", (event) =>{
-    event.preventDefault()
-    console.log("Hello World")
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // console.log("Hello World")
 
-    
-    const checkedInputs = []
+  const checkedInputs = [];
 
-    for(let i = 0; i < form.entertainment.length; i++){
-        if(form.entertainment[i].checked){
-            checkedInputs.push(form.entertainment[i].value)
-        }
+  for (let i = 0; i < form.entertainment.length; i++) {
+    if (form.entertainment[i].checked) {
+      checkedInputs.push(form.entertainment[i].value);
     }
-    
-    alert(checkedInputs)
+  }
 
+  alert(checkedInputs);
 
-})
+  console.log(checkedInputs);
+
+  console.log(form.city.value);
+  console.log(form.gender.value);
+});
