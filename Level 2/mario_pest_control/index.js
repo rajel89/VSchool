f569b62ebeec8b681ae94341037e3ca77376261a
Rajah Ello <rajah.ello@yahoo.com>
2022-11-07 22:09:36 -0600

    mario pest control

---
+++ Level 2/mario_pest_control/index.js
@@ -0,0 +1,48 @@
+
+
+
+// **Pricing:**
+
+// - Goombas: 5 Coins
+// - Bob-ombs: 7 Coins
+// - Cheep-cheeps: 11 Coins
+
+
+
+
+
+// The number of baddies caught will be entered into thier input fields. Each baddies type will have their own input feild.
+//Calcualte cost total number of baddies type multiplied by price
+
+
+function multiplyGoombasBy (){
+    numGoombas = document.getElementById("goombas").value;
+    document.getElementById("totalGoombas").value = (numGoombas * 5);
+}
+
+console.log(multiplyGoombasBy)
+
+
+
+function mutiplyBobombsBy (){
+    numBobombs = document.getElementById("bobombs").value;
+    document.getElementById("totalBobombs").value = (numBobombs * 7);
+}
+
+console.log(mutiplyBobombsBy)
+
+
+
+function multiplyCheepsBy(){
+    numCheeps = document.getElementById("cheep-cheeps").value;
+    document.getElementById("totalCheeps").value = (numCheeps * 11);
+}
+
+
+
+
+
+
+
+
+
