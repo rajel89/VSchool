78069853c26984d58a81bc80d6c0bd240f194b9d
Rajah Ello <rajah.ello@yahoo.com>
2022-11-13 22:32:56 -0600

    updated mario pest control.

--- Level 2/mario_pest_control/index.js
+++ Level 2/mario_pest_control/index.js
@@ -15,6 +15,36 @@
 //Calcualte cost total number of baddies type multiplied by price
 
 
+// const boombInput = document.getElementById("totalGoombasInput").value;
+// const cheepCheepInput = document.getElementById("totalCheep-CheepInput").value;
+// const bobombInput = document.getElementById("totalBob-ombInput").value;
+
+
+
+
+// function multiplyGoombasBy (){
+//     numGoombas = document.getElementById("goombas").value;
+//     document.getElementById("totalGoombas").value = (numGoombas * 5);
+// }
+
+// console.log(multiplyGoombasBy)
+
+
+
+// function mutiplyBobombsBy (){
+//     numBobombs = document.getElementById("bobombs").value;
+//     document.getElementById("totalBobombs").value = (numBobombs * 7);
+// }
+
+// console.log(mutiplyBobombsBy)
+
+
+
+// function multiplyCheepsBy(){
+//     numCheeps = document.getElementById("cheep-cheeps").value;
+//     document.getElementById("totalCheeps").value = (numCheeps * 11);
+// }
+
 function multiplyGoombasBy (){
     numGoombas = document.getElementById("goombas").value;
     document.getElementById("totalGoombas").value = (numGoombas * 5);
@@ -37,6 +67,8 @@ function multiplyCheepsBy(){
     numCheeps = document.getElementById("cheep-cheeps").value;
     document.getElementById("totalCheeps").value = (numCheeps * 11);
 }
+
+
 
 
 
