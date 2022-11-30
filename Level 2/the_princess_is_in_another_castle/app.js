f5b8ee03978dfc3c86a0c38e0b180947f65c9b86
Rajah Ello <rajah.ello@yahoo.com>
2022-11-29 23:21:14 -0600

    added functions for each operations

--- Level 2/the_princess_is_in_another_castle/app.js
+++ Level 2/the_princess_is_in_another_castle/app.js
@@ -11,6 +11,48 @@ class player {
     this.name = ""; //name is equal to blank to allow for dynamic functionality; to allow for Mario or Luigi to be passed
     this.totalCoins = 0; //every player will start with 0
     this.status = status;//"status" of type "String" (Options are Powered Up, Big, Small, and Dead)
-    this.hasstar = hasStar;
+    this.hasStar = hasStar;
   }
 }
+
+//Has a parameter called namePicked where the names "Mario" or "Luigi" is passed
+setName = () => {
+       
+    let namePicked = Math.random() * 2;
+     switch (namePicked) {
+        case 0:
+            this.name = "Mario";
+            break;
+        case 1:
+            this.name = "Luigi"
+            break;
+    } 
+}
+
+//gotHit of type function - this is called whenever the player is hit by an enemy. 
+//gotHit() will set the status property accordingly. (Statuses go from "Powered Up" to "Big" to "Small" to "Dead".)
+
+gotHit(){
+
+}
+
+//gotPowerup of type function - called when a powerup is received and sets the status accordingly. 
+//(Statuses go from "Small" to "Big" to "Powered Up". If you are Powered Up and it hits this function, you get a star)
+
+gotPowerUp(){
+
+}
+
+
+//addCoin of function - adds a coin to totalCoins
+
+
+addCoin()
+
+
+//- `print` of type `function` - prints to the console the name, totalCoins, status, and star properties. 
+//Make sure you make this look nice such as:
+//- Name: Luigi,
+//- Status: Small, etc, etc
+
+print()
