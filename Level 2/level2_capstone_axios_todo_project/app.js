709ced0b15e3558cf208a70ff8196ad56997d232
Rajah Ello <rajah.ello@yahoo.com>
2022-12-02 01:38:17 -0600

    level 2 todo list, PUT function not working for now.

---
+++ Level 2/level2_capstone_axios_todo_project/app.js
@@ -0,0 +1,153 @@
+const todoForm = document.todoform;
+
+todoForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const newTodo = {
+    title: todoForm.title.value,
+    price: todoForm.price.value,
+    description: todoForm.description.value,
+    imgUrl: todoForm.imgUrl.value,
+    completed: false,
+  };
+
+//Part 2 - Post
+  axios
+    .post("https://api.vschool.io/rajel/todo", newTodo)
+    .then((response) => {
+      console.log(response.data);
+      clearAll();
+      getAll();
+    })
+    .catch((error) => console.log(error));
+});
+
+const clearAll = () => {
+  const toDoList = document.getElementById("list");
+  while (toDoList.firstChild) {
+    toDoList.removeChild(toDoList.firstChild);
+  }
+};
+
+const populateToDo = () => {};
+
+//Part 1 - GET
+
+const getAll = () => {
+  axios
+    .get("https://api.vschool.io/rajel/todo")
+    .then((response) => {
+      for (let i = 0; i < response.data.length; i++) {
+        const toDoItem = document.createElement("li");
+        toDoItem.setAttribute("id", `${response.data[i]._id}`);
+        const h2 = document.createElement("h2");
+        const headerValue = response.data[i].title;
+        h2.textContent = headerValue;
+
+        const s = document.createElement("s");
+        s.textContent = headerValue;
+
+        const price = document.createElement("p");
+        const priceValue = `$${response.data[i].price}`;
+        price.textContent = priceValue;
+
+        const p = document.createElement("p");
+        const pValue = response.data[i].description;
+        p.textContent = pValue;
+
+        const img = document.createElement("img");
+        const imgValue = response.data[i].imgUrl;
+        img.src = imgValue;
+        document.getElementById("list").appendChild(img);
+
+
+
+        const updateForm = document.createElement("input");
+        updateForm.placeholder = "Change Info Here";
+
+        // const updateButton = document.createElement('button')
+
+        
+        const updateButton = document.createElement("button");
+        
+
+
+
+        const completeButton = document.createElement("button");
+        const boxCheck = response.data[i].completed;
+        if (boxCheck === false) {
+          toDoItem.appendChild(h2);
+          completeButton.textContent = "Complete";
+        
+        } else if (boxCheck === true) {
+          toDoItem.appendChild(s);
+          completeButton.textContent = "Incomplete";
+       
+        }
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "X";
+
+        toDoItem.appendChild(img);
+        toDoItem.appendChild(price);
+        toDoItem.appendChild(p);
+        toDoItem.appendChild(updateForm);
+        toDoItem.appendChild(completeButton);
+        
+
+        toDoItem.appendChild(deleteButton);
+        completeButton.addEventListener("click", (e) => {
+          completeToDo(e, boxCheck);
+        });
+
+        updateForm.addEventListener("click", (e) => {
+            updateToDo(e);
+          });
+
+
+        deleteButton.addEventListener("click", (e) => {
+          deleteToDo(e);
+        });
+        document.getElementById("list").appendChild(toDoItem);
+      }
+    })
+    .catch((error) => console.log(error));
+};
+
+//Part 3 - PUT Part 1
+const completeToDo = (e, updateTodo) => {
+  console.log(updateTodo);
+  updateTodo = !updateTodo;
+  const updatedToDo = {
+    completed: itemEditEl,
+  };
+  const { id } = e.target.parentElement;
+  axios
+    .put(`https://api.vschool.io/rajel/todo/${id}`, updatedToDo)
+    .then((response) => {
+      console.log(response.data);
+      clearAll();
+      getAll();
+    })
+    .catch((error) => console.log(error));
+};
+
+
+//Part 4 - Delete
+
+const deleteToDo = (e) => {
+  console.log(e);
+  const { id } = e.target.parentElement;
+  axios
+    .delete(`https://api.vschool.io/rajel/todo/${id}`)
+    .then((response) => {
+      console.log(response.data);
+      clearAll();
+      getAll();
+    })
+    .catch((error) => console.log(error));
+};
+
+
+
+
+getAll();
